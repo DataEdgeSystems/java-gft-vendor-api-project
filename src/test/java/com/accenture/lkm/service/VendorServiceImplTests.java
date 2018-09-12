4826d838284f0ea3a54b960da92b399a79c4fe9e
Khozema Nullwala <khozema.nullwala@accenture.com>
2018-09-12 16:20:23 +0530

    VendorService Project Template for Working on ADOP!

---
+++ src/test/java/com/accenture/lkm/service/VendorServiceImplTests.java
@@ -0,0 +1,14 @@
+package com.accenture.lkm.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class VendorServiceImplTests {
+
+	@Test
+	public void test_Sample_Method() {
+		assertEquals("Spring Boot Application Test!","Spring Boot Application Test!");		
+	}	
+	
+}
